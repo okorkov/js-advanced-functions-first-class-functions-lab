8ad595457a724d24d8c27a9d86033c0e9d57ca65
okorkov <alex.okorkov@gmail.com>
2021-02-18 13:19:14 -0800

    commit tree 23586ac01bedc935013d284c0929c2d091d6f62c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arrayOfDrivers) {return [arrayOfDrivers[0], arrayOfDrivers[1]]}
+
+const returnLastTwoDrivers = function(arrayOfDrivers) {return [arrayOfDrivers[arrayOfDrivers.length - 2], arrayOfDrivers[arrayOfDrivers.length -1]]}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(fare) {return function(multiplier=fare) {return fare * multiplier}}
+
+const fareDoubler = function(fare) {return createFareMultiplier(fare)(2)}
+
+const fareTripler = function(fare) {return createFareMultiplier(fare)(3)}
+
+function selectDifferentDrivers(arrayOfDrivers, callBack) {
+  return callBack(arrayOfDrivers)
+}
